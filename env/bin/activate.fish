7ae1987f4c3a18bef9af28c80d1db4e11cfd5404
Ngan Luu <ngan.luu@sjsu.edu>
2020-04-15 18:50:35 -0700

    Edit product.html

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/annluu/Grocery_store/env"
+set -gx VIRTUAL_ENV "/Users/annluu/Downloads/Grocery_store/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
