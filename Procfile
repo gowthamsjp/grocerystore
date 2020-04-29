fee56297152c37e4c0a2a4096c14b18a5bb66b2f
= <=>
2020-04-29 05:14:13 -0700

    make it better

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: gunicorn Grocery_store.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- cmpe133/settings.py
+++ cmpe133/settings.py
@@ -73,6 +73,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "HOST": "localhost",
+        "PORT": "8000",
     }
 }
 
