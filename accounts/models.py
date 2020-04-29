cb6677a06d18dfce3c155e25f8013f9ab40fb874
= <=>
2020-04-29 03:26:54 -0700

    test

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from ecommerce.models import Product, User
 from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.signals import user_logged_in
-from localflavor.us.models import USStateField
+# from localflavor.us.models import USStateField
 
 
 # class UserAddressManager(models.Manager):
@@ -14,7 +14,7 @@ class UserAddress(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     address = models.CharField(max_length=120, null=False )
     city = models.CharField(max_length=50,)
-    state = USStateField(null=True)
+    state = models.CharField(max_length=120,)
     zipcode = models.CharField(max_length=25)
     phone_number = models.CharField(max_length=17, ) # validators should be a list
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
