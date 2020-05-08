c92ce1caf62e48cd0961a04f1f46293827e45e71
AnhMinhHoangLe <leanh6998@gmail.com>
2020-05-08 15:18:16 -0700

    make it better

--- cmpe133/settings.py
+++ cmpe133/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
